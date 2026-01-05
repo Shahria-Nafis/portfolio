17ba06ff1e8c3443e81116862bad09b7ad1b33f8
Shahria Nafis <shahrianafis1@gmail.com>
2026-01-05 20:00:13 +0600

    GVBF

---
+++ components/Social.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/abdurrahman-webdev" },
+  {
+    icon: <FaLinkedinIn />,
+    path: "https://www.linkedin.com/in/abdurrahman-webdev/",
+  },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
